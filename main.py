2d19644a6e46cba59a2ac33325ac58f9cc532004
Alex Faust <alexander.faust@cooper.edu>
2019-12-03 16:33:33 -0500

    cs102-hw03

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+import csv
 
 def main():
     assert len(sys.argv) > 1, "No input file path specified."
@@ -7,8 +7,11 @@ def main():
     input_file_path = sys.argv[1]
     print(f"Processing input file: {input_file_path}")
 
-    # TODO: Fill in the actual logic here!
-
+    with open(input_file_path, mode= 'r') as file:
+        csvread = csv.reader(file, delimiter = ',')
+        for row in csvread:
+            intlist = [float(i) for i in row]
+            print(sum(intlist)/len(intlist))
 
 if __name__ == "__main__":
     main()
